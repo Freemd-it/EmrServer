fddfffe5fccc98eeaf0c3a8e86fb9b78e92bed23
DongyeolLee <dongyeol8@gmail.com>
2017-06-09 17:28:48 +0900

    Make basic format of emrServer

---
+++ app/controllers/testController.js
@@ -0,0 +1,10 @@
+exports.test100 = function(req, res) {
+
+
+    console.log("****************************************8");
+    console.log("in the testController");
+    console.log("****************************************8");
+
+    return;
+
+};
