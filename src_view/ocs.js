6c97b850a37a355804bcd8b2641cb0f74912f4dd
giseoplee <llgs901@naver.com>
2018-02-20 23:01:10 +0900

    (2018-02-20) update by giseop.lee : add datepicker library of semantic ui & update datepicker in ocs page & add history page layout

--- src_view/ocs.js
+++ src_view/ocs.js
@@ -3,19 +3,21 @@ import './vendors/css/bootstrap-theme.css';
 import './vendors/css/semantic.css';
 import './vendors/css/Semantic-UI-Alert.css';
 import './vendors/css/billboard.min.css';
+import './vendors/css/calendar.min.css';
 
 import './vendors/js/bootstrap.js';
 import './vendors/js/semantic.js';
 import './vendors/js/semantic.js';
 import './vendors/js/Semantic-UI-Alert.js';
- 
- 
+import './vendors/js/calendar.min.js';
+
+
 // 바이탈
 import './vendors/js/d3.min.js';
 import './vendors/js/billboard.min.js'
- 
-//css 
+
+//css
 import './resources/css/index.scss';
 
-//접수 
+//접수
 import './resources/js/ocs.js';
